c01dbca5b8700f58b51d3edc8211bc76222834cc
Mallory Cavalheiro <cavalheiro.mallory@proton.me>
2023-01-10 11:08:32 +0100

    password project

---
+++ README.md
@@ -0,0 +1,38 @@
+# Password Generator
+
+This is a solution to Scrimba's solo project. [View Project](https://www.figma.com/file/NEj9JDycMjF3XKXq7swoc9/Random-Password-Generator-(New-version)?node-id=0%3A1&t=26hJo0qzYpkZWv5k-0)
+
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- Mobile-first workflow
+- BEM
+- JavaScript
+
+### Features
+
+Users are able to:
+
+- Generate a password of 15 characters: [a-z][A-Z] (default)
+- Customise the password length
+- Adding numeric characters
+- Add special characters
+- Adding special characters
+- Switching between dark and light mode
+
+### Useful resources
+
+- [Checkbox-JS](https://www.w3schools.com/howto/howto_js_display_checkbox_text.asp)
+- [Toggle-Light-Mode](https://traveling-coderman.net/code/light-mode/)
+- [Copy-on-Click](https://stackoverflow.com/questions/45071353/copy-text-string-on-click)
+
+
+## Author
+
+- Frontend Mentor - [@mallory-cvlh](https://www.frontendmentor.io/profile/mallory-cvlh)
+- Twitter - [@MalloryCvlh](https://twitter.com/MalloryCvlh)
---
+++ js/main.js
@@ -0,0 +1,94 @@
+// Variables
+let firstPassword = document.getElementById("first-password")
+let secondPassword = document.getElementById("second-password")
+let choiceLength = document.getElementById("length")
+let choiceNumber = document.getElementById("number");
+let choiceSpecial = document.getElementById("special-character");
+let errorMessage = document.getElementById("error-message")
+let btnGenerate = document.getElementById("btn-generate")
+let passwordLength = 0
+
+const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+const specials = ["~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "{", "[", "}", "]", ",", "|", ":", ";", "<", ">", ".", "?", "/"]
+const numbersSpecials = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "{", "[", "}", "]", ",", "|", ":", ";", "<", ">", ".", "?", "/"]
+
+function generatePassword() {
+    let firstRandomPwd = ""
+    let secondRandomPwd = ""
+    let characters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+    errorMessage.textContent = ""
+
+
+    // Check choiceLength
+    if (choiceLength.value === '') {
+        passwordLength = 15
+    } else if (choiceLength.value < 5 || choiceLength.value > 18) {
+        errorMessage.textContent = "Select a length from 5 to 18"
+        passwordLength = 0
+    } else {
+        passwordLength = choiceLength.value
+    }
+
+    // Characters based on the users preference
+    if (choiceNumber.checked && choiceSpecial.checked) {
+        for (let i = 0; i < numbersSpecials.length; i++) {
+            characters.push(numbersSpecials[i])
+        }
+    } else if (choiceSpecial.checked) {
+        for (let i = 0; i < specials.length; i++) {
+            characters.push(specials[i])
+        }
+    } else if (choiceNumber.checked) {
+        for (let i = 0; i < numbers.length; i++) {
+            characters.push(numbers[i])
+        }
+    }
+
+    // Password generator
+    for (let i = 0; i < passwordLength; i++) {
+        firstRandomPwd += characters[Math.floor(Math.random() * (characters.length))]
+        secondRandomPwd += characters[Math.floor(Math.random() * (characters.length))]
+    }
+    firstPassword.textContent = firstRandomPwd
+    secondPassword.textContent = secondRandomPwd
+}
+
+// ================= //
+// Toggle light mode //
+// ================= //
+function isLight() {
+    return localStorage.getItem("light-mode");
+}
+
+function toggleRootClass() {
+    document.querySelector(":root").classList.toggle("light");
+}
+
+function toggleLocalStorageItem() {
+    if (isLight()) {
+        localStorage.removeItem("light-mode");
+    } else {
+        localStorage.setItem("light-mode", "set");
+    }
+}
+
+if (isLight()) {
+    toggleRootClass();
+}
+
+document.querySelector(".theme-icon").addEventListener("click", () => {
+    toggleLocalStorageItem();
+    toggleRootClass();
+});
+
+// ============= //
+// COPY ON CLICK //
+// ============= //
+function copy(that) {
+    var inp = document.createElement('input');
+    document.body.appendChild(inp)
+    inp.value = that.textContent
+    inp.select();
+    document.execCommand('copy', false);
+    inp.remove();
+}
